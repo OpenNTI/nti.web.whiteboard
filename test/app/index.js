dd76bf15ed4f5a26174bd1f0da31303ffd5e460c
Cory Jones <cory.jones@nextthought.com>
2018-01-26 11:36:04 -0600

    Add upload ability to editor

--- test/app/index.js
+++ test/app/index.js
@@ -1,9 +1,37 @@
-import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Whiteboard from '../../src/index';
+import {Prompt} from 'nti-web-commons';
+
+// import Whiteboard from '../../src/index';
+import ImageEditor from '../../src/image-editor';
+
+import 'nti-style-common/all.scss';
+import 'nti-web-commons/lib/index.css';
 
 // ReactDOM.render(
 // 	React.createElement(Whiteboard, {}),
 // 	document.getElementById('content')
 // );
+class Test extends React.Component {
+	componentDidMount () {
+
+	}
+
+	constructor (props) {
+		super(props);
+	}
+
+	onClick = () => {
+		Prompt.modal(<ImageEditor/>);
+	}
+
+	render () {
+		return (<div className="launch" onClick={this.onClick}>Launch</div>);
+	}
+}
+
+
+ReactDOM.render(
+	React.createElement(Test, {}),
+	document.getElementById('content')
+);
