dc7b6eb51f38493ce90f5b2ea28891b1d024737f
Cory Jones <cory.jones@nextthought.com>
2018-01-26 11:55:57 -0600

    Move upload into separate component

---
+++ src/image-editor/Upload.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Loading, Input} from 'nti-web-commons';
+
+import {getImgSrc, drawFromImg} from './utils';
+
+export default class Upload extends React.Component {
+	static propTypes = {
+		onChange: PropTypes.func.isRequired
+	}
+
+	attachImgRef = x => this.imgRef = x
+	attachFileRef = x => this.fileInput = x
+
+	state = { loading: false }
+
+	onImgLoad = () => {
+		drawFromImg(this.canvas, this.imgRef);
+	}
+
+	uploadAssets = (file) => {
+		const { onChange } = this.props;
+
+		this.setState({loading: true, file});
+
+		getImgSrc(file).then((imgSrc) => {
+			const img = new Image();
+			img.onload = () => {
+				this.setState({loading: false});
+				onChange && onChange(img);
+			};
+
+			img.src = URL.createObjectURL(file);
+		});
+	}
+
+	render () {
+		if(this.state.loading) {
+			return <Loading.Mask/>;
+		}
+
+		return <Input.File className="asset-file" ref={this.attachFileRef} onFileChange={this.uploadAssets}/>;
+	}
+}
