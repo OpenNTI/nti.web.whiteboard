fb7fd9b36a86e1306ec96b8974ddbaad9377eff0
Cory Jones <cory.jones@nextthought.com>
2018-01-26 11:36:04 -0600

    Add upload ability to editor

--- src/image-editor/View.jsx
+++ src/image-editor/View.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Loading, Input} from 'nti-web-commons';
+
+import {getImgSrc, drawFromImg} from './utils';
 
 export default class ImageEditor extends React.Component {
 	static propTypes = {
 		image: PropTypes.any,
 		editorState: PropTypes.object
 	}
+
+	attachImgRef = x => this.imgRef = x
+	attachFileRef = x => this.fileInput = x
+
 
 
 	setCanvas = x => {
@@ -20,6 +27,7 @@ export default class ImageEditor extends React.Component {
 		const {editorState, image} = this.props;
 
 		this.state = {
+			loading: false,
 			currentState: {
 				image,
 				edits: editorState || {}
@@ -27,11 +35,21 @@ export default class ImageEditor extends React.Component {
 		};
 	}
 
-
 	componentDidUpdate () {
 		this.draw();
 	}
 
+	onImgLoad = () => {
+		drawFromImg(this.canvas, this.imgRef);
+	}
+
+	uploadAssets = (file) => {
+		this.setState({loading: true, file});
+
+		getImgSrc(file).then((imgSrc) => {
+			this.setState({imgSrc, loading: false});
+		});
+	}
 
 	draw () {
 		const {canvas} = this;
@@ -43,11 +61,17 @@ export default class ImageEditor extends React.Component {
 		const {currentState} = this.state;
 
 
+		if(!this.state.file) {
+			return <Input.File className="asset-file" ref={this.attachFileRef} onFileChange={this.uploadAssets}/>;
+		}
 
-		return (
-			<div className="nti-image-editor">
-				<canvas ref={this.setCanvas} />
-			</div>
-		);
+		if(this.state.loading) {
+			return <Loading.Mask/>;
+		}
+
+		return (<div className="nti-image-editor">
+			<canvas ref={this.setCanvas}/>
+			{this.state.imgSrc ? (<img ref={this.attachImgRef} onLoad={this.onImgLoad} src={this.state.imgSrc} style={{display: 'none'}}/>) : null}
+		</div>);
 	}
 }
