bfdd386e371f1ae90f3e3d9178735154a7589d42
Andrew Ligon <andrew.ligon@nextthought.com>
2018-01-26 10:58:07 -0600

    boilerplate

--- src/image-editor/View.jsx
+++ src/image-editor/View.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function ImageEditor () {
-	return (
-		<div>
-			Image Editor
-		</div>
-	);
+export default class ImageEditor extends React.Component {
+	static propTypes = {
+		image: PropTypes.any,
+		editorState: PropTypes.object
+	}
+
+
+	setCanvas = x => {
+		this.canvas = x;
+		this.draw();
+	}
+
+
+	constructor (props) {
+		super(props);
+
+		const {editorState, image} = this.props;
+
+		this.state = {
+			currentState: {
+				image,
+				edits: editorState || {}
+			}
+		};
+	}
+
+
+	componentDidUpdate () {
+		this.draw();
+	}
+
+
+	draw () {
+		const {canvas} = this;
+		const {currentState} = this.state;
+	}
+
+
+	render () {
+		const {currentState} = this.state;
+
+
+
+		return (
+			<div className="nti-image-editor">
+				<canvas ref={this.setCanvas} />
+			</div>
+		);
+	}
 }
