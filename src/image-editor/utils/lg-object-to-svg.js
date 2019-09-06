ef62fe574f84f6fa45dd20f1dba56643b13319a4
Isaac Peterson <isaac.peterson@nextthought.com>
2019-09-06 16:50:24 -0500

    clean up

--- src/image-editor/utils/lg-object-to-svg.js
+++ src/image-editor/utils/lg-object-to-svg.js
@@ -1,13 +1,13 @@
 export default function LgObjectToSvg(lgObject) {
 
     //save from object to string before writing out
-    var svgString = `
+    const svgString = `
         <!-- nti-linear-gradient test -->
         <svg xmlns="http://www.w3.org/2000/svg">
         <defs>
-                <linearGradient id="Gradient" gradientTransform="${lgObject.rotation}" viewBox="0 0 10 1">`
-                + printStops(lgObject) +
-                `</linearGradient>
+                <linearGradient id="Gradient" gradientTransform="${lgObject.rotation}" viewBox="0 0 10 1">
+                ${printStops(lgObject)}
+                </linearGradient>
             </defs>
             <rect x="0" y="0" height="100%" width="100%"  fill="url(#Gradient)" />
         </svg>`;
