b54d23aac4f79e312ce0821ebd994bd6bea9f67e
Isaac Peterson <isaac.peterson@nextthought.com>
2019-09-06 16:45:57 -0500

    eslint clean up

--- src/image-editor/utils/LgObjectToSvg.js
+++ src/image-editor/utils/LgObjectToSvg.js
@@ -1,6 +1,3 @@
-
-//input object
-//output string
 export default function LgObjectToSvg(lgObject) {
 
     //save from object to string before writing out
@@ -9,20 +6,20 @@ export default function LgObjectToSvg(lgObject) {
         <svg xmlns="http://www.w3.org/2000/svg">
         <defs>
                 <linearGradient id="Gradient" gradientTransform="${lgObject.rotation}" viewBox="0 0 10 1">`
-                + printStops(lgObject) + 
+                + printStops(lgObject) +
                 `</linearGradient>
             </defs>
             <rect x="0" y="0" height="100%" width="100%"  fill="url(#Gradient)" />
         </svg>`;
 
-        
+
         return svgString;
     }
-    
+
     const printStops = (lgObject) => {
-        var s = '';
+        const s = '';
         lgObject.stops.map((lg) => {
             s += `<stop offset="${lg.offset}" stopColor="${lg.stopcolor}"/>`
-        })
+        });
         return s;
     }
--- src/image-editor/utils/SvgToLgObject.js
+++ src/image-editor/utils/SvgToLgObject.js
@@ -1,20 +1,17 @@
-
-//input string
-//output object
 export default function SvgToLgObject(svgString) {
 
     if(!svgString.includes("nti-linear-gradient")){
         console.warn("svg file wasnt from nti");
         return null;
-    }
+    };
 
     //find attributes
-    var json = {
+    const json = {
         "rotation":'',
         "stops":[],
     };
 
-    var temp = document.createElement('div');
+    const temp = document.createElement('div');
     temp.innerHTML = svgString;
 
     //get rotation of svg
@@ -25,7 +22,7 @@ export default function SvgToLgObject(svgString) {
     const nodelist = temp.querySelectorAll('stop');
     Array.from(nodelist).forEach((stop) => {
         json.stops.push({"offset": stop.getAttribute('offset'), "stopcolor": stop.getAttribute('stopcolor')});
-    })  
+    });
 
     return json;
 };
