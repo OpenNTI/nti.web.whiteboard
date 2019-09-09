7e8543db887e6ff4fb52b4a2c45595eb1647621c
Isaac Peterson <isaac.peterson@nextthought.com>
2019-09-09 13:36:38 -0500

    eslint clean up

--- src/image-editor/utils/svg-to-gradient-object.js
+++ src/image-editor/utils/svg-to-gradient-object.js
@@ -1,29 +1,27 @@
-export default function SvgToGradientObject(svgString) {
+export default function SvgToGradientObject (svgString) {
 
-    if(!svgString.includes("nti-linear-gradient")){
-        console.warn("svg file wasnt from nti");
-        return null;
-    };
+	if (!svgString.includes('nti-linear-gradient')) {
+		return null;
+	}
 
-    //find attributes
-    const json = {
-        "rotation":"",
-        "stops":[],
-    };
+	//find attributes
+	const json = {
+		'rotation': '',
+		'stops': [],
+	};
 
-    const temp = document.createElement('div');
-    temp.innerHTML = svgString;
+	const temp = document.createElement('div');
+	temp.innerHTML = svgString;
 
-    //get rotation of svg
-    const rotation = temp.querySelector('linearGradient');
-    json.rotation = rotation.getAttribute('gradientTransform');
+	//get rotation of svg
+	const rotation = temp.querySelector('linearGradient');
+	json.rotation = rotation.getAttribute('gradientTransform');
 
-    //make list of stops and add to json
-    const nodelist = temp.querySelectorAll('stop');
-    Array.from(nodelist).forEach((stop) => {
-        json.stops.push({"offset": stop.getAttribute('offset'), "stopcolor": stop.getAttribute('stopcolor')});
-    });
+	//make list of stops and add to json
+	const nodelist = temp.querySelectorAll('stop');
+	Array.from(nodelist).forEach((stop) => {
+		json.stops.push({ 'offset': stop.getAttribute('offset'), 'stopcolor': stop.getAttribute('stopcolor') });
+	});
 
-    return json;
-};
-
+	return json;
+}
