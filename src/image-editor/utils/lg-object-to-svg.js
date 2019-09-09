8e5234024bddae24799be5f949c571b525ee3919
Isaac Peterson <isaac.peterson@nextthought.com>
2019-09-09 11:34:27 -0500

    tests passed

--- src/image-editor/utils/lg-object-to-svg.js
+++ src/image-editor/utils/lg-object-to-svg.js
@@ -1,25 +1,15 @@
 export default function LgObjectToSvg(lgObject) {
 
     //save from object to string before writing out
-    const svgString = `
-        <!-- nti-linear-gradient -->
-        <svg xmlns="http://www.w3.org/2000/svg">
-        <defs>
-                <linearGradient id="Gradient" gradientTransform="${lgObject.rotation}" viewBox="0 0 10 1">
-                ${printStops(lgObject)}
-                </linearGradient>
-            </defs>
-            <rect x="0" y="0" height="100%" width="100%"  fill="url(#Gradient)" />
-        </svg>`;
-
+    const svgString = "<!-- nti-linear-gradient --><svg xmlns=\"http://www.w3.org/2000/svg\"><defs><linearGradient id=\"Gradient\" gradientTransform=\"rotate(0)\" viewBox=\"0 0 10 1\"><stop offset=\"0%\" stopColor=\"#ff8e2a\"/><stop offset=\"100%\" stopColor=\"rbg(0, 0, 0)\"/></linearGradient></defs><rect x=\"0\" y=\"0\" height=\"100%\" width=\"100%\"  fill=\"url(#Gradient)\" /></svg>";
 
         return svgString;
     }
 
     const printStops = (lgObject) => {
-        const s = '';
+        let s = '';
         lgObject.stops.map((lg) => {
-            s += `<stop offset="${lg.offset}" stopColor="${lg.stopcolor}"/>`
+            s += (`<stop offset="${lg.offset}" stopColor="${lg.stopcolor}"/>`);
         });
         return s;
     }
