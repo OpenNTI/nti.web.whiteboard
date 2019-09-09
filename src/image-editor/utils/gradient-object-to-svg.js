5b93bd6f68f6f7aa54abcc4cbf6baa18d5972a7c
Isaac Peterson <isaac.peterson@nextthought.com>
2019-09-09 13:36:38 -0500

    eslint clean up

--- src/image-editor/utils/gradient-object-to-svg.js
+++ src/image-editor/utils/gradient-object-to-svg.js
@@ -1,15 +1,17 @@
-export default function GradientObjectToSvg(gradientObject) {
+export default function GradientObjectToSvg (gradientObject) {
 
-    //save from object to string before writing out
-    const svgString = `<!-- nti-linear-gradient --><svg xmlns="http://www.w3.org/2000/svg"><defs><linearGradient id="Gradient" gradientTransform="rotate(0)" viewBox="0 0 10 1">${printStops(gradientObject)}<linearGradient></defs><rect x="0" y="0" height="100%" width="100%"  fill="url(#Gradient)" /></svg>`;
+	//save from object to string before writing out
+	const svgString = `<!-- nti-linear-gradient --><svg xmlns="http://www.w3.org/2000/svg"><defs><linearGradient id="Gradient" gradientTransform="rotate(0)" viewBox="0 0 10 1">${printStops(gradientObject)}<linearGradient></defs><rect x="0" y="0" height="100%" width="100%"  fill="url(#Gradient)" /></svg>`;
 
-        return svgString;
-    }
+	return svgString;
+}
 
-    const printStops = (gradientObject) => {
-        let s = '';
-        gradientObject.stops.map((lg) => {
-            s += (`<stop offset="${lg.offset}" stopColor="${lg.stopcolor}"/>`);
-        });
-        return s;
-    }
+const printStops = (gradientObject) => {
+	let s = '';
+	gradientObject.stops.map((lg) => {
+		s += (`<stop offset="${lg.offset}" stopColor="${lg.stopcolor}"/>`);
+	});
+	return s;
+
+};
+
