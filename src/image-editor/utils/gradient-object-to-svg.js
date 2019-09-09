89761c6d7ef3a0ce9024be52f0cf8fe09ff53458
Isaac Peterson <isaac.peterson@nextthought.com>
2019-09-09 12:04:43 -0500

    name changes

--- src/image-editor/utils/gradient-object-to-svg.js
+++ src/image-editor/utils/gradient-object-to-svg.js
@@ -1,4 +1,4 @@
-export default function LgObjectToSvg(lgObject) {
+export default function GradientObjectToSvg(gradientObject) {
 
     //save from object to string before writing out
     const svgString = "<!-- nti-linear-gradient --><svg xmlns=\"http://www.w3.org/2000/svg\"><defs><linearGradient id=\"Gradient\" gradientTransform=\"rotate(0)\" viewBox=\"0 0 10 1\"><stop offset=\"0%\" stopColor=\"#ff8e2a\"/><stop offset=\"100%\" stopColor=\"rbg(0, 0, 0)\"/></linearGradient></defs><rect x=\"0\" y=\"0\" height=\"100%\" width=\"100%\"  fill=\"url(#Gradient)\" /></svg>";
@@ -6,9 +6,9 @@ export default function LgObjectToSvg(lgObject) {
         return svgString;
     }
 
-    const printStops = (lgObject) => {
+    const printStops = (gradientObject) => {
         let s = '';
-        lgObject.stops.map((lg) => {
+        gradientObject.stops.map((lg) => {
             s += (`<stop offset="${lg.offset}" stopColor="${lg.stopcolor}"/>`);
         });
         return s;
