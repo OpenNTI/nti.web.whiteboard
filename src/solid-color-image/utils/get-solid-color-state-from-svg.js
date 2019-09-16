bfe036d435fe792dbddb0cb435b1df4fbd6200b6
Andrew Ligon <andrew.ligon@nextthought.com>
2019-09-16 11:44:32 -0500

    fill out utility to get the solid color info from an svg

--- src/solid-color-image/utils/get-solid-color-state-from-svg.js
+++ src/solid-color-image/utils/get-solid-color-state-from-svg.js
@@ -1,5 +1,17 @@
+import {Color} from '@nti/lib-commons';
+
+import {SVG_IDENTIFIER} from '../Constants';
+
 export default function getSolidColorStateFromSVG (svg) {
 	if (!svg) { return null; }
+	if (svg.indexOf(SVG_IDENTIFIER) === -1) { return null; }
 
-	//TODO: fill this in
+	const scratch = document.createElement('div');
+
+	scratch.innerHTML = svg;
+	
+	const rect = scratch.querySelector('rect');
+	const fill = rect && rect.getAttribute('fill');
+
+	return fill ? {color: Color(fill)} : null;
 }
