cc76ce7ba3546c19297847b3f8d10573e79df320
Andrew Ligon <andrew.ligon@nextthought.com>
2019-09-18 18:31:07 -0500

    add text input and presets for solid color images

--- src/solid-color-image/Editor.jsx
+++ src/solid-color-image/Editor.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
-import {Input} from '@nti/web-commons';
+import {scoped} from '@nti/lib-locale';
+import {Color as LibColor} from '@nti/lib-commons';
+import {Input, Text} from '@nti/web-commons';
 
 import Styles from './Editor.css';
 
 const {Color} = Input;
 const cx = classnames.bind(Styles);
+const t = scoped('nti-whiteboard.solid-color-image.Editor', {
+	presets: 'Presets'
+});
+
+const Presets = [
+	{color: LibColor.fromHex('#D10615')},
+	{color: LibColor.fromHex('#F6A807')},
+	{color: LibColor.fromHex('#F7EA00')},
+	{color: LibColor.fromHex('#7AD500')},
+	{color: LibColor.fromHex('#3F7600')},
+	{color: LibColor.fromHex('#BF00E3')},
+	{color: LibColor.fromHex('#9300FF')},
+	{color: LibColor.fromHex('#4B8DE4')},
+	{color: LibColor.fromHex('#49E4C1')},
+	{color: LibColor.fromHex('#B6EB82')},
+	{color: LibColor.fromHex('#8C5827')},
+	{color: LibColor.fromHex('#8C5827')},
+	{color: LibColor.fromHex('#000000')},
+	{color: LibColor.fromHex('#4A4A4A')},
+	{color: LibColor.fromHex('#9B9B9B')},
+	{color: LibColor.fromHex('#FFFFFF')},
+];
 
 export default class SolidColorEditor extends React.Component {
 	static propTypes = {
@@ -31,7 +55,12 @@ export default class SolidColorEditor extends React.Component {
 		return (
 			<div className={cx('solid-color-editor')}>
 				<Color.SaturationBrightness value={color} onChange={this.onChange} />
-				<Color.Hue value={color} onChange={this.onChange} />
+				<div className={cx('container')}>
+					<Color.Hue value={color} onChange={this.onChange} />
+					<Color.Text className={cx('text-input')} value={color} onChange={this.onChange} />
+					<Text.Base className={cx('preset-label')}>{t('presets')}</Text.Base>
+					<Color.PresetSwatches swatches={Presets} selected={color} onSelect={this.onChange} />
+				</div>
 			</div>
 		);
 	}
