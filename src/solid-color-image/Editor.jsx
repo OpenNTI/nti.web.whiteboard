8a684be2c19e7bf403144b5b55244678449f6302
Andrew Ligon <andrew.ligon@nextthought.com>
2019-09-18 20:30:52 -0500

    add titles to the color presets

--- src/solid-color-image/Editor.jsx
+++ src/solid-color-image/Editor.jsx
@@ -10,26 +10,42 @@ import Styles from './Editor.css';
 const {Color} = Input;
 const cx = classnames.bind(Styles);
 const t = scoped('nti-whiteboard.solid-color-image.Editor', {
-	presets: 'Presets'
+	presets: 'Presets',
+	red: 'Red',
+	orange: 'Orange',
+	yellow: 'Yellow',
+	lightGreen: 'Light Green',
+	darkGreen: 'Dark Green',
+	lightPurple: 'Light Purple',
+	darkPurple: 'Dark Purple',
+	blue: 'Blue',
+	cyan: 'Cyan',
+	pastelGreen: 'Pastel Green',
+	brown: 'Brown',
+	blueGrey: 'Blue Grey',
+	black: 'Black',
+	darkGrey: 'Dark Grey',
+	lightGrey: 'Light Grey',
+	white: 'White'
 });
 
 const Presets = [
-	{color: LibColor.fromHex('#D10615')},
-	{color: LibColor.fromHex('#F6A807')},
-	{color: LibColor.fromHex('#F7EA00')},
-	{color: LibColor.fromHex('#7AD500')},
-	{color: LibColor.fromHex('#3F7600')},
-	{color: LibColor.fromHex('#BF00E3')},
-	{color: LibColor.fromHex('#9300FF')},
-	{color: LibColor.fromHex('#4B8DE4')},
-	{color: LibColor.fromHex('#49E4C1')},
-	{color: LibColor.fromHex('#B6EB82')},
-	{color: LibColor.fromHex('#8C5827')},
-	{color: LibColor.fromHex('#8C5827')},
-	{color: LibColor.fromHex('#000000')},
-	{color: LibColor.fromHex('#4A4A4A')},
-	{color: LibColor.fromHex('#9B9B9B')},
-	{color: LibColor.fromHex('#FFFFFF')},
+	{color: LibColor.fromHex('#D10615'), title: t('red')},
+	{color: LibColor.fromHex('#F6A807'), title: t('orange')},
+	{color: LibColor.fromHex('#F7EA00'), title: t('yellow')},
+	{color: LibColor.fromHex('#7AD500'), title: t('lightGreen')},
+	{color: LibColor.fromHex('#3F7600'), title: t('darkGreen')},
+	{color: LibColor.fromHex('#BF00E3'), title: t('lightPurple')},
+	{color: LibColor.fromHex('#9300FF'), title: t('darkPurple')},
+	{color: LibColor.fromHex('#4B8DE4'), title: t('blue')},
+	{color: LibColor.fromHex('#49E4C1'), title: t('cyan')},
+	{color: LibColor.fromHex('#B6EB82'), title: t('pastelGreen')},
+	{color: LibColor.fromHex('#8C5827'), title: t('brown')},
+	{color: LibColor.fromHex('#575990'), title: t('blueGrey')},
+	{color: LibColor.fromHex('#000000'), title: t('black')},
+	{color: LibColor.fromHex('#4A4A4A'), title: t('darkGrey')},
+	{color: LibColor.fromHex('#9B9B9B'), title: t('lightGrey')},
+	{color: LibColor.fromHex('#FFFFFF'), title: t('white')},
 ];
 
 export default class SolidColorEditor extends React.Component {
