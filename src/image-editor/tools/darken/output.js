5e9f377481d0167da37bb769056049596d417975
Andrew Ligon <andrew.ligon@nextthought.com>
2020-04-29 11:14:14 -0500

    handle zero opacity on the darkening

--- src/image-editor/tools/darken/output.js
+++ src/image-editor/tools/darken/output.js
@@ -4,10 +4,10 @@ export default {
 	after (ctx, formatting, layout) {
 		const {darken} = formatting;
 
-		if (!darken || !darken.color) { return; }
+		if (!darken || !darken.color || darken.opacity === 0) { return; }
 
 		const color = Color(darken.color);
-		const alpha = darken.opacity || 1;
+		const alpha = darken.opacity ?? 1;
 
 		ctx.save();
 		ctx.setTransform(1, 0, 0, 1, 0, 0);
