cbb540153fb9f6b099381e0b017fd3d6ff8cf8bc
nti-jarvis <support@nextthought.com>
2020-09-15 15:19:48 +0000

    :wrench: Synchronize project files from updated templates

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,3 +4,5 @@ import '@nti/style-common/all.scss';
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+window.$AppConfig = window.$AppConfig || { server: '/dataserver2/' };
